432b53e353d43609a66f1a83ed91884e3d3d88e6
HealthyJunkie <androwas101@gmail.com>
2021-03-14 04:57:06 +0200

    ...

--- src/Components/Helpers/Loading.jsx
+++ src/Components/Helpers/Loading.jsx
@@ -2,8 +2,6 @@ import React from 'react'
 
 export default function Loading(props) {
     return (
-        <div>
             <img className={`${props.toShow ? "animate-loading" : ""} loading`} alt="loading" src="loading.gif"/>
-        </div>
     )
 }
